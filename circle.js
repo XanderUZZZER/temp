fc3de5b1dcf4b7b8e58a94c1341991d7c11f0f73
XanderUZZZER <xanderuzzzer@gmail.com>
2020-06-11 15:29:31 +0300

    js added

---
+++ circle.js
@@ -0,0 +1,5 @@
+const { PI } = Math;
+
+export function area(r) { return PI * r ** 2; }
+
+export function circumference(r) { return 2 * PI * r; }
---
+++ index.js
@@ -0,0 +1,2 @@
+import { area } from './circle.js';
+console.log(`The area of a circle of radius 4 is ${area(4)}`);
