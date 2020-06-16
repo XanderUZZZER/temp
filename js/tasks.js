1588060e533797e8e583a9eab8a32deccdbfa25d
XanderUZZZER <xanderuzzzer@gmail.com>
2020-06-16 23:23:22 +0300

    lex envir

--- js/tasks.js
+++ js/tasks.js
@@ -1,5 +1,22 @@
-function foo() {
-    Promise.resolve().then(foo);
-  }
+// function foo() {
+//     Promise.resolve().then(foo);
+//   }
   
-  foo();
+//   foo();
+
+function makeCounter() {
+  let count = 0;
+  return function() {
+    return count++;
+  };
+}
+
+let counter1 = makeCounter();
+let counter2 = makeCounter();
+
+console.log( counter1() ); // 0
+console.log( counter1() ); // 1
+
+console.log( counter2() ); // 0 (независимо)
+
+console.log( makeCounter ); // 1
